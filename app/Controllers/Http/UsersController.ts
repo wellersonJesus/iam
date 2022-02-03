bea449e5a4ca4701114946913859c67d8d4d9d99
wellersonjesus <wellersonjesus@hotmail.com>
2022-02-03 08:13:14 -0300

    feat/ user controller

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,8 +1,10 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
 
 export default class UsersController {
-  public async store({ response }: HttpContextContract) {
-    return response.created({})
+  public async store({ request, response }: HttpContextContract) {
+    const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+    const user = await User.create(userPayload)
+    return response.created({ user })
   }
 }
