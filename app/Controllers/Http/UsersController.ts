0738ef47e2768151413a7d41d552c13683db4bf7
wellersonjesus <wellersonjesus@hotmail.com>
2022-02-03 10:06:00 -0300

    feat /UserController - index.ts - user.spect

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -4,6 +4,10 @@ import User from 'App/Models/User';
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+    const UserByEmail = await User.findBy('email', userPayload.email)
+
+    if (UserByEmail) return response.conflict({ message: 'email alread in use' })
+
     const user = await User.create(userPayload)
     return response.created({ user })
   }
--- test/Users/user.spec.ts
+++ test/Users/user.spec.ts
@@ -1,9 +1,11 @@
+import { UserFactory } from './../../database/factories/index';
 import test from "japa"
+import { Assert } from "japa/build/src/Assert"
 import supertest from 'supertest'
+import Database from '@ioc:Adonis/Lucid/Database';
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
-//Deve Criar um usuario
 /*
 {
   "users": {
@@ -14,8 +16,9 @@ const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
   }
 }
 */
-test.group('User', () => {
-  test.only('it should create an user', async (assert) => {
+//Deve Criar um usuario
+test.group('User', (group) => {
+  test('it should create an user', async (assert) => {
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
@@ -28,6 +31,25 @@ test.group('User', () => {
     assert.equal(body.user.email, userPayload.email)
     assert.equal(body.user.username, userPayload.username)
     assert.equal(body.user.avatar, userPayload.avatar)
-    assert.notExists(body.user.password, 'password defined')
+    assert.notExists(body.user.password, 'Password defined')
+  })
+  //Retorna erro 409 quando email já estiver sendo utilizado
+  test.only('it should return 409 whe email is already in use', async (assert) => {
+    const { email } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL)
+      .post('/users')
+      .send({
+        email,
+        username: 'test',
+        password: 'test',
+      })
+      .expect(409)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
   })
 })
