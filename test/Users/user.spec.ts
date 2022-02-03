2950fc852503ae2ad037a36093e89540c58b28f2
wellersonjesus <wellersonjesus@hotmail.com>
2022-02-03 16:20:23 -0300

    feat / Erro teste atualizar user

--- test/Users/user.spec.ts
+++ test/Users/user.spec.ts
@@ -104,6 +104,27 @@ test.group('User', (group) => {
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 422)
   })
+  //Atualizar um usuário
+  test('it should update an user', async (assert) => {
+    const { id, password } = await UserFactory.create()
+    const email = 'test@test.com'
+    const avatar = 'http://github.com/wellersonjesus.png'
+
+    const { body } = await supertest(BASE_URL)
+      .put('/users/${id}')
+      .send({
+        email,
+        avatar,
+        password,
+      })
+      .expect(200)
+
+    assert.exists(body.user, 'User underfined')
+    assert.equal(body.user.email, email)
+    assert.equal(body.user.avatar, avatar)
+    assert.equal(body.user.id, id)
+  })
+
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })
