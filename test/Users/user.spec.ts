b5b7f90ecf04d191a216ee91c565d788038334a3
wellersonjesus <wellersonjesus@hotmail.com>
2022-02-04 08:35:35 -0300

    feat /Atualização senha do usuario

--- test/Users/user.spec.ts
+++ test/Users/user.spec.ts
@@ -2,6 +2,8 @@ import Database from '@ioc:Adonis/Lucid/Database'
 import { UserFactory } from 'Database/factories/'
 import test from 'japa'
 import supertest from 'supertest'
+import Hash from '@ioc:Adonis/Core/Hash'
+
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
@@ -123,6 +125,30 @@ test.group('User', (group) => {
     assert.equal(body.user.email, email)
     assert.equal(body.user.avatar, avatar)
     assert.equal(body.user.id, id)
+  })
+  //Atualiza senha do usuário
+  test('it should update the password of the user', async (assert) => {
+    const user = await UserFactory.create()
+    const password = 'test'
+
+    const { body } = await supertest(BASE_URL)
+      .put(`/users/${user.id}`)
+      .send({
+        email: user.email,
+        avatar: user.avatar,
+        password,
+      })
+      .expect(200)
+
+    assert.exists(body.user, 'User undefined')
+    assert.equal(body.user.id, user.id)
+
+    await user.refresh()
+    assert.isTrue(await Hash.verify(user.password, password))
+
+    await user.refresh()
+    assert.isTrue(await Hash.verify(user.password, password))
+
   })
 
   group.beforeEach(async () => {
